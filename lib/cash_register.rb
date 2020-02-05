4ef20239d73b16878a75b1e45fd83d0d76d33ccf
Adam G Horn <hornadam8@gmail.com>
2020-02-05 13:05:44 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,25 @@
-
+class CashRegister
+  
+  attr_accessor :total , :discount
+  
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+  end
+  
+  def total
+    return @total
+  end
+  
+  def add_item(item,price,num = 1)
+    value = price * num
+    @total += value
+  end
+  
+  def apply_discount
+    percent = (100 - @discount)/100
+    @total * percent
+    @total
+  end
+    
+end
